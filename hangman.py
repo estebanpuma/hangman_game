cc1aba65a826a4628a8f3860c20b68bdf4062a2e
Esteban Puma <estepuma94@gmail.com>
2022-01-03 14:26:03 -0500

    Se crea el juego inicialmente  nivel funcional ok

--- hangman.py
+++ hangman.py
@@ -1,5 +1,6 @@
 import os, time
 
+#Welcome to the game
 def start():
     welcome = '''                  **            ** ****** **     ****** ******* **     ** ******
                    **    **    **  **     **     **     **   ** ***   *** **
@@ -21,10 +22,12 @@ def start():
              '''
     start_w = "A jugar"
     print(welcome)
-    time.sleep(3)
+    time.sleep(1)
     os.system ("cls")
     print(start_w)
-
+    time.sleep(1)
+    os.system ("cls")
+#Max time for each game
 def timeout():
     timer = 120
     for s in range (120):
@@ -35,11 +38,11 @@ def timeout():
         if timer == 0:
             print("game over")
 
-
+#player info
 def player():
     player = input('Ingresa tu nombre: ')
     
-
+# get data
 def get_data():
     words = []
     with open('./files/data.txt','r', encoding= 'utf-8') as f:
@@ -49,25 +52,62 @@ def get_data():
 
 
 def interface(words):
-    word = words[0]
-    guessed=[]
-    cont = 0
-    for letter in word:
-        guessed.append('_')
-        print(guessed[cont], end=' ')
-        cont += 1
-    letterin = ''
+# char control (only lower letters)   
+    word = list(words[3])
+    special = ['á', 'é', 'í', 'ó', 'ú']
+    standar = ['a', 'e', 'i', 'o', 'u']
+    for i in range(len(word)):
+        cont = 0
+        for char in special:
+            
+            if word[i] == char:
+                word[i] = standar[cont]
+            cont += 1    
+    print(word)
+
+#show total spaces for the word
+    guess_status = ['_' for letter in word ]
+    for space in guess_status:
+        print(space, end=' ')
+    print("")
+    #create a list with the acepted chars
+    acepted_chars = [chr(i) for i in range(97,122) ] + list('ñ')
+    #game cycle
+    input_letters = ''
     while True:
-        
-        letter_guess = input('Ingresa una letra: ')
-        letterin += letter_guess
-        for letter in word:
-            if letter_guess == letter:
-                print(letter, end=' ')
+        #input control
+        while True:
+            try:
+                
+                guessed_letter = (input('Ingresa una letra: ')).lower()
+                os.system ("cls") 
+                if guessed_letter not in acepted_chars:
+                    raise TypeError
+            except TypeError:
+                print('Recuerda que solo debes ingresar letras')
+            if len(guessed_letter) > 1:
+                print('Solo ingresa una letra')
+                
             else:
-                print('_', end=' ')
-        if word == letter_guess:
+                break  
+        
+            
+        input_letters += ' ' + guessed_letter
+        
+        
+        print('Letras ingresadas', input_letters)
+        for i in range(len(word)):
+            if guessed_letter == word[i]:
+                guess_status[i] = guessed_letter  
+        for space in guess_status:
+            print(space, end=' ')
+        print("")
+
+        if guess_status == list(word):
             break
+
+    print('Felicidades')
+
 
 
 def run():
